a75d7a50b5460e8bdc47d401c5675d95c5a9825b
Nabeelah <nabeelahyousuph@gmail.com>
2019-06-03 14:28:47 +0100

    Style search bar

--- instagram-app/src/components/SearchBar/Nav.js
+++ instagram-app/src/components/SearchBar/Nav.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Nav = ({url, icon}) => {
-    return <a href={url}><i class={icon}></i></a> 
+    return <a href={url}><span>{icon}</span></a> 
 }
 
 export default Nav;
--- instagram-app/src/components/SearchBar/SearchBar.js
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import Nav from './Nav';
+import React from "react";
+import Nav from "./Nav";
+import './SearchBar.css';
+import { IconContext } from "react-icons";
+import { FiInstagram, FiCompass, FiHeart, FiUser} from "react-icons/fi";
 
-const SearchBar = ({ icon }) => {
-    return (
+const SearchBar = () => {
+  return (
+      <IconContext.Provider value={{className: 'react-icon'}}>
+    <div className='nav-bar'>
+
+      <div className= 'icons'>
         <div>
-            
+          <Nav url="#" icon={<FiInstagram />} />
         </div>
-    )
+        <h3>Instagram</h3>
+      </div>
 
-}
+      <div className='search-input'>
+        <input input="text" placeholder="Search" />
+      </div>
+
+      <div>
+        <Nav url="#" icon={<FiCompass />} />
+        <Nav url="#" icon={<FiHeart />} />
+        <Nav url="#" icon={<FiUser />} />
+      </div>
+
+    </div>
+    </IconContext.Provider>
+  );
+};
 
 export default SearchBar;
